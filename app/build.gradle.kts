0653ea0ca449012d405d3b9e7d5a366ae8a8de37
Aman <theaman1805@gmail.com>
2026-07-07 19:10:05 +0530

    bump versionCode to 2 for Google Play closed testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.hisuperaman.wallety"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
