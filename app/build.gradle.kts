879dddc24c72430d08391d4ed395e88cd27f3929
Aman <theaman1805@gmail.com>
2026-07-07 19:25:21 +0530

    switch back to versionCode 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.hisuperaman.wallety"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
